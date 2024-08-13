c3f740911f1d72f37b6c87491783e85820b1d820
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-08-12 23:31:41 -0400

    Initial commit

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { BookCard } from "../book-card/book-card";
+import { BookView } from "../book-view/book-view";
+
+export const MainView = () => {
+  const [books, setBooks] = useState([]);
+
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  useEffect(() => {
+    fetch("https://openlibrary.org/search.json?q=star+wars")
+      .then((response) => response.json())
+      .then((data) => {
+        const booksFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+            author: doc.author_name?.[0],
+          };
+        });
+
+        setBooks(booksFromApi);
+      });
+  }, []);
+
+  if (selectedBook) {
+    return (
+      <BookView book={selectedBook} onBackClick={() => setSelectedBook(null)} />
+    );
+  }
+
+  if (books.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          book={book}
+          onBookClick={(newSelectedBook) => {
+            setSelectedBook(newSelectedBook);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
